6bddf0abd2ea25dd323610b04e88d075dc5610d8
svxx6 <sarithvimukthix@gmail.com>
2025-07-29 18:47:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&DlIASRZQ#icKQ-at24IX5Kg_ISoFbFqVqdkZt_kRbXCulQO-LCHc' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&eohHUKBK#c9qOJWJPiL-aVPrPtCQFKuf2HOVjgqTrQRUql8ZPHD0' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
